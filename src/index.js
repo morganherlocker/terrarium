c800d61f4d890d935924479c2d922120e0e763fd
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-20 11:30:25 -0400

    added example map and layers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-module.exports = function(){
+module.exports = (function(){
   $(function(){
     //$('body').css({'background-color':'red'})
-    $('#legend').css({'background-color':'blue'})
+    //$('#manager').css({'background-color':'blue'})
     $('#map').css({'background-color':'orange'})
   })
-}
+})()
